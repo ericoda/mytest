9751cd789947dc7053bfd5f96fc1ea266225ea6d
EricLiu <ericliu@hyx.com>
2016-06-21 22:31:14 +0800

    build prj

---
+++ test.drawerEffect/test.drawerEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test.drawerEffect
+//
+//  Created by EricLau on 16/6/21.
+//  Copyright © 2016年 ericoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test.drawerEffect/test.drawerEffect/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test.drawerEffect
+//
+//  Created by EricLau on 16/6/21.
+//  Copyright © 2016年 ericoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
