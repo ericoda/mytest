ffe1f1b4b2a484d0462ef26722e3a89c64457f38
Eric <ericliu@hyx.com>
2016-09-30 17:12:43 +0800

    1.  约束动态控制视图

---
+++ test_controlHiddenByConstraint/test_controlHiddenByConstraint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_controlHiddenByConstraint
+//
+//  Created by Eric on 16/9/30.
+//  Copyright © 2016年 ericoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_controlHiddenByConstraint/test_controlHiddenByConstraint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_controlHiddenByConstraint
+//
+//  Created by Eric on 16/9/30.
+//  Copyright © 2016年 ericoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
